c8faecb842a6f55790708b5dc750f20524f89c52
Danilo Costa de Carvalho <danilo.advance2011@gmail.com>
2024-05-03 19:17:30 -0300

    feat: add new color to default theme, the success color #328a03

--- src/styles/themes/default.ts
+++ src/styles/themes/default.ts
@@ -3,6 +3,7 @@ export const defaultTheme = {
     white: '#fff',
     '--primary-background': '#1976d2',
     '--secondary-background': '#f5f5f5',
+    '--success-color': '#328a03',
     '--brand-color': '#000000DE', 
     '--danger-color': '#F72A3C',
 } as const
