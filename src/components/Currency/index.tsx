addacb2c94fdaedb63bed7e1eb20e63cd9e66991
Danilo Costa de Carvalho <danilo.advance2011@gmail.com>
2024-05-03 19:03:48 -0300

    feat: create the currency input component

---
+++ src/components/Currency/index.tsx
@@ -0,0 +1,32 @@
+import { CurrencyInput } from 'react-currency-mask';
+import { Control, Controller } from 'react-hook-form';
+
+interface CurrencyProps {
+  control: Control<{
+    title: string;
+    subtitle: string;
+    prize: string;
+    price: number;
+    raffleDate: Date;
+    units: number;
+  }, any>
+};
+
+export function Currency({ control }: CurrencyProps) {
+
+  return (
+    <Controller
+      name="price"
+      control={control}
+      render={({ field }) => (
+        <CurrencyInput
+          value={field.value}
+          onChangeValue={(_, value) => {
+            field.onChange(value);
+          }}
+          InputElement={<input type='text' placeholder='R$ Valor da rifa' />}
+        />
+      )}
+    />
+  );
+};
